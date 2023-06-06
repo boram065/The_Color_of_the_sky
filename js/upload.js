c406d45dcde6afba2c757f57476619784c347dca
hyeg0121 <hyeg0121@naver.com>
2023-06-06 13:44:43 +0900

    [TODO] TODO 메모

--- js/upload.js
+++ js/upload.js
@@ -8,3 +8,9 @@ fileUploadFeild.addEventListener("click", () => {
     img.style.width = `100%`;
     img.src = "images/postCover1.png";
 });
+
+// TODO : 유효성 검사 하기 
+// 비밀번호 4자리, 빈칸 있는지 없는지
+
+// TODO : 업로드 하면 다시 record.html 로 이동하기
+// 업로드한 게시물 보여지게 하기
