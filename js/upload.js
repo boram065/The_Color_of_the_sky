1b0b837105e855c79eb7734f68c9cbcce489d3a3
hyeg <hyeg0121@naver.com>
2023-06-13 15:10:10 +0900

    [feat] upload 할 때 유효성 검사
    제목 입력, 비밀번호 입력, 사진 업로드 빈칸 체크

--- js/upload.js
+++ js/upload.js
@@ -1,6 +1,8 @@
 const uploadBtn = document.querySelector("#upload_btn");
-const img = document.getElementById("upload_img");
+const titleInput= document.querySelector("#title_input");
+const pwInput = document.querySelector("#pw_input");
 const fileUploadFeild = document.querySelector(".fileUploadField");
+const img = document.getElementById("upload_img");
 
 fileUploadFeild.addEventListener("click", () => {
     fileUploadFeild.querySelector("span").innerHTML = "";
@@ -11,6 +13,32 @@ fileUploadFeild.addEventListener("click", () => {
 
 // TODO : 유효성 검사 하기 
 // 비밀번호 4자리, 빈칸 있는지 없는지
-
 // TODO : 업로드 하면 다시 record.html 로 이동하기
 // 업로드한 게시물 보여지게 하기
+uploadBtn.addEventListener("click", ()=>{
+    let title = titleInput.value;
+    let pw = pwInput.value;
+
+    if(title.length === 0){
+        alert("제목을 입력해주세요.");
+        return;
+    }
+
+    if(img.src.length == 0){
+        alert("이미지를 업로드해주세요.");
+        return;
+    }
+
+    if(pw.length === 0){
+        alert("비밀번호을 입력해주세요.");
+        return;
+    }
+
+    // TODO: 숫자가 아닐 경우
+    if(pw.length < 4){
+        alert("비밀번호는 4자리 이상 숫자입니다.");
+    }
+
+    // TODO: open하고 현재 창 닫기
+    window.open("../record.html");  
+});
