91db735f4868fc07d2918759a41d92af2784c6f5
hyeg0121 <hyeg0121@naver.com>
2023-06-20 23:17:46 +0900

    [refactor] nav style에서 분리 & 수정

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+const navToggleDiv = document.getElementById("nav_toggle");
+const navToggleI = nav_toggle.getElementsByTagName("i")[0];
+const navListUl = document.getElementsByClassName("nav-list")[0];
+
+nav_toggle.onclick = () => {
+    navToggleI.classList.toggle("fa-bars");
+    navToggleI.classList.toggle("fa-xmark");
+    navListUl.classList.toggle("show-menu");
+};
