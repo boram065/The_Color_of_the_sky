9eb47d39d95fee523d8e3715b4d5e6f312d40ac3
hyeg0121 <hyeg0121@naver.com>
2023-06-22 00:09:43 +0900

    [feat] 삭제 페이지로 이동

--- js/post.js
+++ js/post.js
@@ -3,4 +3,5 @@ let imgIndex = 1;
 for(let i in postsDiv){
     postsDiv[i].getElementsByTagName("img")[0].src = `images/postcover/postCover${imgIndex}.jpg`;
     imgIndex++;
+    postsDiv[i].onclick = () => {window.open("../delete.html")};
 }
