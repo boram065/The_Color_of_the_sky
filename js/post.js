d25e22eaa4d3336a87e37803babee0ff9f0f0ea7
hyeg0121 <hyeg0121@naver.com>
2023-06-23 22:06:40 +0900

    [refactor] 이미지 경로와 타이틀 innerHTML을 작성한 js 삭제
    페이지 로드 시 오류가 발생했기 때문

--- js/post.js
+++ js/post.js
@@ -1,33 +1,33 @@
-let titles = [
-    "하늘의 품에서 피어난 감동",
-    "끝없는 하늘, 끝나지 않는 나의 하루",
-    "하늘과 함께 춤추는 순간들",
-    "하늘의 색깔, 나의 감정을 담아서",
-    "구름 위를 걸으며 느껴진 행복",
-    "하늘에 묻은 나의 소망들",
-    "하늘을 이불삼아"
-];
+// let titles = [
+//     "하늘의 품에서 피어난 감동",
+//     "끝없는 하늘, 끝나지 않는 나의 하루",
+//     "하늘과 함께 춤추는 순간들",
+//     "하늘의 색깔, 나의 감정을 담아서",
+//     "구름 위를 걸으며 느껴진 행복",
+//     "하늘에 묻은 나의 소망들",
+//     "하늘을 이불삼아"
+// ];
 
-const goUploadDiv = document.getElementById("go-upload");
-let postsDiv = document.getElementsByClassName("post");
-let imgIndex = 1;
+// const goUploadDiv = document.getElementById("go-upload");
+// let postsDiv = document.getElementsByClassName("post");
+// let imgIndex = 1;
 
-for(let i in postsDiv){
-    // 이미지 경로
-    postsDiv[i].getElementsByTagName("img")[0].src = `images/postcover/postCover${imgIndex}.jpg`;
-    imgIndex++;
+// for(let i in postsDiv){
+//     // 이미지 경로
+//     postsDiv[i].getElementsByTagName("img")[0].src = `images/postcover/postCover${imgIndex}.jpg`;
+//     imgIndex++;
 
-    // title 지정
-    postsDiv[i].getElementsByClassName("title")[0].innerHTML = titles[i];
+//     // title 지정
+//     postsDiv[i].getElementsByClassName("title")[0].innerHTML = titles[i];
 
-    // uploaded_record일 때 
-    if(postsDiv.length > 7){
-        postsDiv[0].getElementsByTagName("img")[0].src = `images/postcover/postCover9.jpg`;
-        postsDiv[0].getElementsByClassName("title")[0].innerHTML = "오늘 하늘이 맑다";
-        postsDiv[i].getElementsByClassName("title")[0].innerHTML = titles[i-1];
-    }
-}
+//     // uploaded_record일 때 
+//     if(postsDiv.length > 7){
+//         postsDiv[0].getElementsByTagName("img")[0].src = `images/postcover/postCover9.jpg`;
+//         postsDiv[0].getElementsByClassName("title")[0].innerHTML = "오늘 하늘이 맑다";
+//         postsDiv[i].getElementsByClassName("title")[0].innerHTML = titles[i-1];
+//     }
+// }
 
-goUploadDiv.onclick = () => {
-    window.open("../upload.html", "_top");
-};
+// goUploadDiv.onclick = () => {
+//     window.open("../upload.html", "_top");
+// };
